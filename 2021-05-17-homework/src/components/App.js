9f61fe6c68cae46bf6920e51506aeaf2e5bddbf3
Gunel99 <45911420+Gunel99@users.noreply.github.com>
2021-05-18 01:55:43 +0400

    Complated

---
+++ 2021-05-17-homework/src/components/App.js
@@ -0,0 +1,11 @@
+import Exchange from "./Exchange/Exchange";
+
+function App() {
+  return (
+    <div className="App">
+      <Exchange />
+    </div>
+  );
+}
+
+export default App;
---
+++ 2021-05-17-homework/src/components/Exchange/Exchange.js
@@ -0,0 +1,90 @@
+import { createRef } from "react";
+import rates from "./rates.json";
+
+function Exchange() {
+  const inputCurRef = createRef();
+  const inputCurCodeRef = createRef();
+  const outputCurRef = createRef();
+  const outputCurCodeRef = createRef();
+
+  const exchange = () => {
+    // const inputCurCodeValRef = inputCurCodeRef.current.value;
+    // const outputCurCodeValRef = outputCurCodeRef.current.value;
+    
+    // const inputCurVal = inputCurRef.currency.value;
+    // const outputCurVal = outputCurRef.current.value;
+
+    const input = rates.find(rate => rate.code === inputCurCodeRef.current.value).value;
+    const output = rates.find(rate => rate.code === outputCurCodeRef.current.value).value;
+
+    outputCurRef.current.value = ((input * inputCurRef.current.value) / output).toFixed(4);
+  };
+
+  return (
+    <div id="exchanger" className="py-4">
+      <div className="container">
+        <div className="row">
+          <form>
+            <div className="mb-3">
+              <label className="form-label">From</label>
+              <div className="d-flex">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="from"
+                  defaultValue="1"
+                  ref={inputCurRef}
+                />
+                <select
+                  className="form-select"
+                  defaultValue="USD"
+                  ref={inputCurCodeRef}
+                >
+                  {rates.map((c, key) => 
+                    <option value={c.code} key={key}>
+                      {c.code}
+                    </option>
+                  )}
+                </select>
+              </div>
+            </div>
+            <img
+              src={process.env.PUBLIC_URL + "../imgs/switch.png"}
+              style={{ width: "100%" }}
+            />
+            <div className="mb-3">
+              <label className="form-label">To</label>
+              <div className="d-flex">
+                <input
+                  type="number"
+                  className="form-control"
+                  id="to" defaultValue="1.7002"
+                  ref={outputCurRef}
+                  disabled
+                />
+                <select 
+                  className="form-select"
+                  defaultValue="AZN"
+                  ref={outputCurCodeRef}
+                >
+                  {rates.map((c, key) => 
+                    <option value={c.code} key={key}>
+                      {c.code}
+                    </option>
+                  )}
+                </select>
+              </div>
+            </div>
+            <div className="mt-5 mb-3">
+              <button className="btn btn-primary" onClick={exchange}>
+                Exchange
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Exchange;
